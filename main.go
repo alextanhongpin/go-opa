b03699af1dc54e2c243696682a6bcd9659f039ad
alextanhongpin <alextan220990@gmail.com>
2020-06-25 00:56:41 +0800

    chore: update example, isolate store

--- main.go
+++ main.go
@@ -8,25 +8,28 @@ import (
 	"log"
 
 	"github.com/open-policy-agent/opa/rego"
+	"github.com/open-policy-agent/opa/storage"
+	"github.com/open-policy-agent/opa/storage/inmem"
+	"github.com/open-policy-agent/opa/util"
 )
 
 func main() {
+	{
+		input := `{"method": "GET", "path": ["salary", "bob"], "user_id": "bob"}`
+		run(input)
+	}
+	{
+
+		input := `{"method": "GET", "path": ["salary", "bob"], "user_id": "alice"}`
+		run(input)
+	}
+}
+
+func run(rawInput string) {
 	ctx := context.Background()
 
 	module := `
 package example
-
-management_chain = {
-  "management_chain": {
-    "bob": [
-      "ken",
-      "janet"
-    ],
-    "alice": [
-      "janet"
-    ]
-  }
-}
 
 default allow = false
 allow {
@@ -38,14 +41,12 @@ allow {
 allow {
 	input.method = "GET"
 	input.path = ["salary", id]
-	managers = management_chain[id]
+	managers = data.management_chain[id]
 	input.user_id = managers[_]
 }
 `
 
-	//raw := `{"method": "GET", "path": ["salary", "bob"], "user_id": "bob"}`
-	raw := `{"method": "GET", "path": ["salary", "bob"], "user_id": "alice"}`
-	d := json.NewDecoder(bytes.NewBufferString(raw))
+	d := json.NewDecoder(bytes.NewBufferString(rawInput))
 	// Numeric values must be represented using json.Number.
 	d.UseNumber()
 
@@ -54,16 +55,44 @@ allow {
 		panic(err)
 	}
 
-	rego := rego.New(
+	r := rego.New(
 		rego.Module("example.rego", module),
+		rego.Store(newStore()),
 		rego.Query("data.example.allow == true"),
 		rego.Input(input),
 	)
-	rs, err := rego.Eval(ctx)
+	rs, err := r.Eval(ctx)
 	if err != nil {
 		log.Fatal("evalErr:", err)
 	}
 	fmt.Println(len(rs))
 	fmt.Printf("%+v\n", rs)
 	fmt.Println(rs[0].Expressions[0])
+}
+
+// Creates a new inmem store for the data required for rules.
+func newStore() storage.Store {
+	data := `{
+  "management_chain": {
+    "bob": [
+      "ken",
+      "janet"
+    ],
+    "alice": [
+      "janet"
+    ]
+  }
+}`
+
+	var json map[string]interface{}
+
+	err := util.UnmarshalJSON([]byte(data), &json)
+	if err != nil {
+		// Handle error.
+	}
+
+	// Manually create the storage layer. inmem.NewFromObject returns an
+	// in-memory store containing the supplied data.
+	store := inmem.NewFromObject(json)
+	return store
 }
